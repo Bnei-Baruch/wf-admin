c3b58dedfbb47097c34bab83607c343f4169a9ea
amnonbb <amnonbb@gmail.com>
2018-04-09 10:49:45 +0300

    add trimmed table

--- src/components/Ingest/IngestCapture.js
+++ src/components/Ingest/IngestCapture.js
@@ -1,14 +1,16 @@
-import React, { Component } from 'react'
+import React, {Component, Fragment} from 'react'
 import DatePicker from 'react-datepicker';
 import moment from 'moment';
-import { getData } from '../shared/tools';
-import { Grid, Segment, Modal, Dropdown } from 'semantic-ui-react'
+import {getData, IVAL} from '../shared/tools';
+import { Container, Grid, Segment, Modal, Dropdown, Icon, Table, Loader } from 'semantic-ui-react'
 import MediaTrimmer from "../Trimmer/MediaTrimmer";
 
 class IngestCapture extends Component {
 
     state = {
+        active: null,
         ingest: [],
+        trimmed: [],
         open: false,
         capture_src: "main",
         capture_file: null,
@@ -23,6 +25,12 @@ class IngestCapture extends Component {
             if (JSON.stringify(this.state.ingest) !== JSON.stringify(data))
                 this.setState({ingest: data});
         });
+        setInterval(() =>
+            getData('trim', (data) => {
+                if (JSON.stringify(this.state.trimmed) !== JSON.stringify(data))
+                    this.setState({trimmed: data})
+            }), 1000
+        );
     };
 
     handleDateChange = (data) => {
@@ -48,6 +56,12 @@ class IngestCapture extends Component {
         this.setState({open: false});
     };
 
+    handleClick = (data) => {
+        console.log(":: Selected trim: ",data);
+        //this.props.onUidSelect(unit);
+        this.setState({active: data.trim_id});
+    };
+
     render() {
 
         const options = [
@@ -61,8 +75,38 @@ class IngestCapture extends Component {
             return ({ key: id, text: name, value: i })
         });
 
+        let trimmed = this.state.trimmed.map((data) => {
+            let name = (data.wfstatus.trimmed) ? data.file_name : <div><Loader size='mini' active inline></Loader>&nbsp;&nbsp;&nbsp;{data.file_name}</div>;
+            let censor = (data.wfstatus.censored) ? <Icon name='copyright'/> : "";
+            let time = moment.unix(data.trim_id.substr(1)).format("HH:mm:ss") || "";
+            //let removed = data.wfstatus.removed ? <Icon name='checkmark'/> : <Icon name='close'/>;
+            if(this.props.removed && data.wfstatus.removed)
+                return;
+            let renamed = data.wfstatus.renamed ? <Icon name='checkmark'/> : <Icon name='close'/>;
+            //let checked = data.wfstatus.checked ? <Icon name='checkmark'/> : <Icon name='close'/>;
+            let buffer = data.wfstatus.buffer ? <Icon name='checkmark'/> : <Icon name='close'/>;
+            let wfsend = data.wfstatus.wfsend ? <Icon name='checkmark'/> : <Icon name='close'/>;
+            let rowcolor = data.wfstatus.censored && !data.wfstatus.checked;
+            let active = this.state.active === data.trim_id ? 'active' : '';
+            return (
+                <Table.Row
+                    negative={rowcolor}
+                    positive={data.wfstatus.wfsend}
+                    warning={!data.wfstatus.trimmed}
+                    className={active} key={data.trim_id} onClick={() => this.handleClick(data)}
+                >
+                    <Table.Cell>{censor}{name}</Table.Cell>
+                    <Table.Cell>{time}</Table.Cell>
+                    <Table.Cell>{renamed}</Table.Cell>
+                    <Table.Cell>{buffer}</Table.Cell>
+                    <Table.Cell negative={!data.wfstatus.wfsend}>{wfsend}</Table.Cell>
+                </Table.Row>
+            )
+        });
+
         return (
-            <Segment textAlign='left'>
+            <Fragment>
+            <Segment textAlign='left' className="ingest_segment">
                 <Grid>
                     <Grid.Column width={2}>
                         <Dropdown
@@ -112,6 +156,24 @@ class IngestCapture extends Component {
                     <MediaTrimmer source={this.state.source} />
                 </Modal>
             </Segment>
+                <Segment textAlign='left' className="ingest_segment">
+                <Table selectable compact='very' basic structured className="ingest_table">
+                    <Table.Header>
+                        <Table.Row className='table_header'>
+                            <Table.HeaderCell>File Name</Table.HeaderCell>
+                            <Table.HeaderCell width={2}>Time</Table.HeaderCell>
+                            <Table.HeaderCell width={1}>RNM</Table.HeaderCell>
+                            <Table.HeaderCell width={1}>BUF</Table.HeaderCell>
+                            <Table.HeaderCell width={1}>SND</Table.HeaderCell>
+                        </Table.Row>
+                    </Table.Header>
+
+                    <Table.Body>
+                        {trimmed}
+                    </Table.Body>
+                </Table>
+                </Segment>
+            </Fragment>
         );
     }
 }
