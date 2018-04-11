62c2ff86cc4d9c14b89fd6cc0a1071da63a5f57f
amnonbb <amnonbb@gmail.com>
2018-04-12 02:24:08 +0300

    Add new tabs, fixes

--- src/App.js
+++ src/App.js
@@ -8,6 +8,8 @@ import 'react-datepicker/dist/react-datepicker.css';
 import './App.css';
 import MonitorApp from "./components/Monitor/MonitorApp";
 import IngestApp from "./components/Ingest/IngestApp";
+import CensorApp from "./components/Censor/CensorApp";
+import AdminApp from "./components/Admin/AdminApp";
 
 class App extends Component {
   render() {
@@ -15,8 +17,8 @@ class App extends Component {
       const panes = [
           { menuItem: { key: 'ingest', icon: 'record', content: 'Ingest' }, render: () => <Tab.Pane attached={true} ><IngestApp /></Tab.Pane> },
           { menuItem: { key: 'monitor', icon: 'eye', content: 'Monitor' }, render: () => <Tab.Pane attached={true} ><MonitorApp /></Tab.Pane> },
-          { menuItem: { key: 'censor', icon: 'copyright', content: 'Censor', disabled: true }, render: () => <Tab.Pane attached={false} disabled>Censor</Tab.Pane> },
-          { menuItem: { key: 'admin', icon: 'detective', content: 'Admin', disabled: true }, render: () => <Tab.Pane attached={false} disabled>Admin</Tab.Pane> },
+          { menuItem: { key: 'censor', icon: 'copyright', content: 'Censor', disabled: false }, render: () => <Tab.Pane attached={false} ><CensorApp/></Tab.Pane> },
+          { menuItem: { key: 'admin', icon: 'detective', content: 'Admin', disabled: false }, render: () => <Tab.Pane attached={false} ><AdminApp/></Tab.Pane> },
       ];
 
     return (
--- src/components/Admin/AdminTrimmed.js
+++ src/components/Admin/AdminTrimmed.js
@@ -172,7 +172,7 @@ class AdminTrimmed extends Component {
                     </Menu.Item>
                     <Menu.Menu position='left'>
                         <Menu.Item>
-                            <Modal trigger={<Button disabled={this.state.disabled} color='blue' onClick={this.openCit} >Rename</Button>} open={this.state.open} closeIcon="close" mountNode={document.getElementById("cit-modal-mount")}>
+                            <Modal trigger={<Button disabled={this.state.disabled} color='blue' onClick={this.openCit} >Rename</Button>} onClose={this.onCancel} open={this.state.open} closeIcon="close" mountNode={document.getElementById("cit-modal-mount")}>
                                 <Modal.Content>
                                     <CIT metadata={this.state.file_data.line} onCancel={this.onCancel} onComplete={(x) => this.onComplete(x)}/>
                                 </Modal.Content>
