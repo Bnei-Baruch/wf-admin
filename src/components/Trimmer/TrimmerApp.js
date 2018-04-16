0e5ff89c5728379ed5f5d2c7ce920d3a812580be
amnonbb <amnonbb@gmail.com>
2018-04-16 20:44:02 +0300

    TrimmedApp: code refactor

--- src/components/Trimmer/TrimmerApp.js
+++ src/components/Trimmer/TrimmerApp.js
@@ -19,63 +19,78 @@ export default class TrimmerApp extends Component {
     componentDidMount() {
         console.log("-- MediaTrimmer Did Mount: ", this.props.file_data);
         if(this.props.source_meta === "trimmed") {
+            // Make meta from trimmmed files
             let data = this.props.file_data;
             let wfid = "t"+moment().format('X');
             let date = moment.unix(wfid.substr(1)).format("YYYY-MM-DD");
             let line = data.line;
             let originalsha1 = data.original.format.sha1;
             let proxysha1 = data.proxy.format.sha1;
-            //let inouts = {"inpoints": []};
-            //let outpoints = {"outpoints": []};
             let filename = data.file_name;
             let censored = this.props.mode === "censor";
             let buffer = this.props.mode === "wfadmin";
             let secured = data.wfstatus.secured;
-            let wfsend = data.wfstatus.wfsend;
-            let fixed = !data.wfstatus.wfsend;
             let trim_meta = {
-                "trim_id":wfid, "date":date, "file_name":filename,
-                "parent": { "id": data.trim_id, "capture_id": data.parent.capture_id, "original_sha1": originalsha1, "proxy_sha1": proxysha1, "file_name":filename, "source":this.props.source_meta },
-                "line":line, "inpoints": data.inpoints, "outpoints": data.outpoints,
-                "wfstatus": { "aricha":false, "buffer":buffer, "fixed":fixed, "trimmed":false, "renamed":false, "wfsend":wfsend, "removed":false, "kmedia":false, "backup":false, "metus":false, "censored":censored, "secured":secured }
-                };
+                trim_id:wfid, date:date, file_name:filename,
+                parent: {
+                    id: data.trim_id,
+                    capture_id: data.parent.capture_id,
+                    original_sha1: originalsha1,
+                    proxy_sha1: proxysha1,
+                    file_name:filename,
+                    source:this.props.source_meta
+                },
+                line: line, inpoints: [], outpoints: [],
+                wfstatus: {
+                    aricha: false,
+                    buffer: buffer,
+                    fixed: false,
+                    trimmed: false,
+                    renamed: false,
+                    wfsend: false,
+                    removed: false,
+                    kmedia: false,
+                    backup: false,
+                    metus: false,
+                    censored:censored,
+                    secured:secured
+                }
+            };
             this.setState({trim_meta: trim_meta});
         } else {
+            // Make meta from captured files
             let data = this.props.file_data;
             let wfid = "t" + moment().format('X');
             let date = moment.unix(wfid.substr(1)).format("YYYY-MM-DD");
             let line = data.line;
             let originalsha1 = data.original.format.sha1;
             let proxysha1 = data.proxy.format.sha1;
-            //let inouts = {"inpoints": []};
-            //let outpoints = {"outpoints": []};
             let filename = data.stop_name;
-            //let censored = this.props.mode === "censor";
             let buffer = this.props.mode === "wfadmin";
             let secured = data.wfstatus.secured;
             let trim_meta = {
-                "trim_id": wfid, "date": date, "file_name": filename,
-                "parent": {
-                    "id": data.capture_id,
-                    "capture_id": data.capture_id,
-                    "original_sha1": originalsha1,
-                    "proxy_sha1": proxysha1,
-                    "file_name": filename,
-                    "source": this.props.source_meta
+                trim_id: wfid, date: date, file_name: filename,
+                parent: {
+                    id: data.capture_id,
+                    capture_id: data.capture_id,
+                    original_sha1: originalsha1,
+                    proxy_sha1: proxysha1,
+                    file_name: filename,
+                    source: this.props.source_meta
                 },
-                "line": line, "inpoints": [], "outpoints": [],
-                "wfstatus": {
-                    "aricha": false,
-                    "buffer": buffer,
-                    "trimmed": false,
-                    "renamed": false,
-                    "wfsend": false,
-                    "removed": false,
-                    "kmedia": false,
-                    "backup": false,
-                    "metus": false,
-                    "censored": false,
-                    "secured": secured
+                line: line, inpoints: [], outpoints: [],
+                wfstatus: {
+                    aricha: false,
+                    buffer: buffer,
+                    trimmed: false,
+                    renamed: false,
+                    wfsend: false,
+                    removed: false,
+                    kmedia: false,
+                    backup: false,
+                    metus: false,
+                    censored: false,
+                    secured: secured
                 }
             };
             this.setState({trim_meta: trim_meta});
@@ -104,11 +119,11 @@ export default class TrimmerApp extends Component {
             this.setState({ loading: false });
             this.props.closeModal();
         }, 5000);
-        putData('http://wfdb.bbdomain.org:8080/trimmer/'+this.state.trim_meta.trim_id, this.state.trim_meta, (cb) => {
-            console.log(":: PUT Respond: ",cb);
+        putData(`http://wfdb.bbdomain.org:8080/trimmer/${this.state.trim_meta.trim_id}`, this.state.trim_meta, (cb) => {
+            console.log(":: Post trim meta: ",cb);
             let lelomikud = this.state.lelomikud ? 1 : 0;
             // FIXME: When API change this must be error recovering
-            fetch("http://wfserver.bbdomain.org:8080/hooks/trim?id="+wfid+"&spc="+lelomikud);
+            fetch(`http://wfserver.bbdomain.org:8080/hooks/trim?id=${wfid}&spc=${lelomikud}`);
         });
     };
 
@@ -116,29 +131,59 @@ export default class TrimmerApp extends Component {
 
     render() {
         return (
-            <Table className="table_main">
+            <Table className='table_main'>
                 <Table.Row>
-                    <Table.Cell width={5} className="table_media">
-                        <MediaPlayer player={this.getPlayer} source={this.props.source} />
+                    <Table.Cell
+                        width={5}
+                        className='table_media'>
+                        <MediaPlayer
+                            player={this.getPlayer}
+                            source={this.props.source} />
                     </Table.Cell>
-                    <Table.Cell width={1} className="table_ctls">
-                        <TrimmerControls player={this.state.player}/>
+                    <Table.Cell
+                        width={1}
+                        className='table_ctls'>
+                        <TrimmerControls
+                            player={this.state.player} />
                     </Table.Cell>
-                    <Table.Cell width={3} className="table_inouts">
-                        <Segment raised textAlign='center' className="inout_content">
-                            <InoutControls onSetPoints={this.getInouts} player={this.state.player} inpoints={this.state.trim_meta.inpoints} outpoints={this.state.trim_meta.outpoints}/>
+                    <Table.Cell
+                        width={3}
+                        className='table_inouts'>
+                        <Segment
+                            raised
+                            textAlign='center'
+                            className='inout_content'>
+                            <InoutControls
+                                onSetPoints={this.getInouts}
+                                player={this.state.player}
+                                inpoints={this.state.trim_meta.inpoints}
+                                outpoints={this.state.trim_meta.outpoints} />
                         </Segment>
                     </Table.Cell>
                 </Table.Row>
                 <Table.Row>
                     <Table.Cell>
-                        <Segment color='blue' textAlign='center' raised ><b>{this.state.trim_meta.file_name}</b></Segment>
+                        <Segment
+                            color='blue'
+                            textAlign='center'
+                            raised ><b>{this.state.trim_meta.file_name}</b>
+                        </Segment>
                     </Table.Cell>
                     <Table.Cell>
-                        <Checkbox label='LeloMikud' onClick={this.toggleLelomikud} checked={this.state.lelomikud} />
+                        <Checkbox
+                            label='LeloMikud'
+                            onClick={this.toggleLelomikud}
+                            checked={this.state.lelomikud} />
                     </Table.Cell>
-                    <Table.Cell textAlign='center'>
-                        <Button size='big' color='red' disabled={!this.state.ioValid} onClick={this.postTrimMeta} loading={this.state.loading} >Trim</Button>
+                    <Table.Cell
+                        textAlign='center'>
+                        <Button
+                            size='big'
+                            color='red'
+                            disabled={!this.state.ioValid}
+                            onClick={this.postTrimMeta}
+                            loading={this.state.loading} >Trim
+                        </Button>
                     </Table.Cell>
                 </Table.Row>
             </Table>
