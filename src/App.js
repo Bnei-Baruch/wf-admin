dd98c3b76f0abc61b13d494a9a6908ed86b392a0
amnonbb <amnonbb@gmail.com>
2018-04-20 18:47:36 +0300

    App: make sure default state tabs is disabled

--- src/App.js
+++ src/App.js
@@ -21,11 +21,11 @@ class App extends Component {
         user: null,
         disabled: true,
         loading: true,
-        wf_ingest: false,
-        wf_censor: false,
-        wf_admin: false,
-        wf_aricha: false,
-        wf_public: false,
+        wf_ingest: true,
+        wf_censor: true,
+        wf_admin: true,
+        wf_aricha: true,
+        wf_public: true,
 
     };
 
@@ -48,13 +48,13 @@ class App extends Component {
     };
 
     checkPermission = (user) => {
-        let wf_public = (user.roles.filter(role => role === 'bb_user').length) > 0;
-        let wf_ingest = (user.roles.filter(role => role === 'wf_ingest').length) > 0;
-        let wf_censor = (user.roles.filter(role => role === 'wf_censor').length) > 0;
-        let wf_admin = (user.roles.filter(role => role === 'wf_admin').length) > 0;
-        let wf_aricha = (user.roles.filter(role => role === 'wf_aricha').length) > 0;
+        let wf_public = user.roles.filter(role => role === 'bb_user').length === 0;
+        let wf_ingest = user.roles.filter(role => role === 'wf_ingest').length === 0;
+        let wf_censor = user.roles.filter(role => role === 'wf_censor').length === 0;
+        let wf_admin = user.roles.filter(role => role === 'wf_admin').length === 0;
+        let wf_aricha = user.roles.filter(role => role === 'wf_aricha').length === 0;
         console.log(":: App - got user: ", user);
-        if(wf_public) {
+        if(!wf_public) {
             this.setState({user, wf_public, wf_admin, wf_censor, wf_ingest, wf_aricha});
         } else {
             alert("Access denied!");
@@ -64,26 +64,27 @@ class App extends Component {
 
   render() {
 
+      const {wf_public,wf_ingest,wf_censor,wf_admin,wf_aricha} = this.state;
       let login = (<LoginPage user={this.state.user} loading={this.state.loading} />);
 
       const panes = [
           { menuItem: { key: 'Home', icon: 'home', content: 'Home', disabled: false },
               render: () => <Tab.Pane attached={true} >{login}</Tab.Pane> },
-          { menuItem: { key: 'ingest', icon: 'record', content: 'Ingest', disabled: !this.state.wf_ingest },
+          { menuItem: { key: 'ingest', icon: 'record', content: 'Ingest', disabled: wf_ingest },
               render: () => <Tab.Pane attached={false} ><IngestApp user={this.state.user} /></Tab.Pane> },
-          { menuItem: { key: 'censor', icon: 'copyright', content: 'Censor', disabled: !this.state.wf_censor },
+          { menuItem: { key: 'censor', icon: 'copyright', content: 'Censor', disabled: wf_censor },
               render: () => <Tab.Pane attached={false} ><CensorApp user={this.state.user} /></Tab.Pane> },
-          { menuItem: { key: 'admin', icon: 'detective', content: 'Admin', disabled: !this.state.wf_admin },
+          { menuItem: { key: 'admin', icon: 'detective', content: 'Admin', disabled: wf_admin },
               render: () => <Tab.Pane attached={false} ><AdminApp user={this.state.user} /></Tab.Pane> },
-          { menuItem: { key: 'monitor', icon: 'eye', content: 'Monitor', disabled: !this.state.wf_public },
+          { menuItem: { key: 'monitor', icon: 'eye', content: 'Monitor', disabled: wf_public },
               render: () => <Tab.Pane attached={false} ><MonitorApp user={this.state.user} /></Tab.Pane> },
-          { menuItem: { key: 'aricha', icon: 'edit', content: 'Aricha', disabled: !this.state.wf_aricha },
+          { menuItem: { key: 'aricha', icon: 'edit', content: 'Aricha', disabled: wf_aricha },
               render: () => <Tab.Pane attached={false} ><ArichaApp user={this.state.user} /></Tab.Pane> },
-          { menuItem: { key: 'dgima', icon: 'film', content: 'Dgima', disabled: !this.state.wf_aricha },
+          { menuItem: { key: 'dgima', icon: 'film', content: 'Dgima', disabled: wf_aricha },
               render: () => <Tab.Pane attached={false} >Dgima</Tab.Pane> },
-          { menuItem: { key: 'insert', icon: 'archive', content: 'Insert', disabled: !this.state.wf_aricha },
+          { menuItem: { key: 'insert', icon: 'archive', content: 'Insert', disabled: wf_aricha },
               render: () => <Tab.Pane attached={false} ><MainPage user={this.state.user} /></Tab.Pane> },
-          { menuItem: { key: 'wfdb', icon: 'settings', content: 'Status', disabled: !this.state.wf_admin },
+          { menuItem: { key: 'wfdb', icon: 'settings', content: 'Status', disabled: wf_admin },
               render: () => <Tab.Pane attached={false} ><WFDB /></Tab.Pane> },
       ];
 
