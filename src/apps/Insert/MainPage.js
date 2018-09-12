27a07d6c414785cd7c22a23a7012984a295a020b
amnonbb <amnonbb@gmail.com>
2018-09-12 14:02:09 +0300

    Fix insert url back

--- src/apps/Insert/MainPage.js
+++ src/apps/Insert/MainPage.js
@@ -45,7 +45,7 @@ class App extends Component {
         console.log(":: Setting metadata from:", filedata);
         const {sha1,size,filename,type,url} = filedata;
         let line = {content_type: null, upload_filename: filename, mime_type: type,
-            url: window.location.href`${url}`};
+            url: window.location.href + `${WFSRV_BACKEND}${url}`};
         let metadata = {sha1, size, line, content_type: "", language: null,
             send_uid: "", upload_type: "", insert_type: this.state.insert};
 
