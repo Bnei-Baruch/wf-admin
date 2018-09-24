5f2cdb6ba2f66dd3a6d8eedee1a0d798603466bd
amnonbb <amnonbb@gmail.com>
2018-09-24 16:04:30 +0300

    WFDBIngest: Add change status and week_date line

--- src/apps/WFDB/WFDBIngest.js
+++ src/apps/WFDB/WFDBIngest.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react'
-import { getData } from '../../shared/tools';
-import { Icon, Table, Container, Loader } from 'semantic-ui-react'
+import {getData, postData, WFDB_BACKEND} from '../../shared/tools';
+import { Icon, Table, Container, Loader, Popup, Checkbox, Input, Button, Label } from 'semantic-ui-react'
 
 class WFDBIngest extends Component {
 
     state = {
         ingest: [],
+        wfstatus: {},
+        line: {},
     };
 
     componentDidMount() {
@@ -28,11 +30,53 @@ class WFDBIngest extends Component {
         });
     };
 
+    getStatus = (data) => {
+        console.log(":: Got status: ",data);
+        this.setState({wfstatus: {...data.wfstatus}, id: data.capture_id})
+    };
+
+    getLine = (data) => {
+        console.log(":: Got status: ",data);
+        this.setState({line: {...data.line}, id: data.capture_id, value: data.line.week_date})
+    };
+
+    setLine = () => {
+        let {line, id, value} = this.state;
+        line.week_date = value;
+        console.log(":: Save Line: ",line);
+        postData(`${WFDB_BACKEND}/ingest/${id}/line`, line, (cb) => {
+            console.log(":: POST Line in WFDB: ",cb);
+        });
+    };
+
+    toggle = (data) => {
+        console.log(":: Got state: ",data + " : ",this.state.wfstatus[data]);
+        let wfstatus = this.state.wfstatus;
+        wfstatus[data] = !this.state.wfstatus[data];
+        fetch(`${WFDB_BACKEND}/ingest/${this.state.id}/wfstatus/${data}?value=${wfstatus[data]}`, { method: 'POST',});
+        this.setState({wfstatus: {...wfstatus}});
+    };
+
     render() {
         let v = (<Icon name='checkmark'/>);
         let x = (<Icon name='close'/>);
         let d = (<Icon color='blue' name='lock'/>);
         let s = (<Icon color='red' name='key'/>);
+
+        let root =(
+            <div><Checkbox label='Capwf' onClick={() => this.toggle("capwf")} checked={this.state.wfstatus.capwf} /><br />
+                <Checkbox label='Trimmed' onClick={() => this.toggle("trimmed")} checked={this.state.wfstatus.trimmed} /><br />
+                <Checkbox label='Locked' onClick={() => this.toggle("locked")} checked={this.state.wfstatus.locked} /><br />
+                <Checkbox label='Secured' onClick={() => this.toggle("secured")} checked={this.state.wfstatus.secured} /><br /></div>
+        );
+
+        let week_date = (
+            <Input type='text' labelPosition='left' action
+                   value={this.state.value} onChange={e => this.setState({value: e.target.value})}>
+                <Label basic>Date:</Label><input className='input_line' />
+                <Button role='button' onClick={this.setLine} disabled={!this.state.value}>Save</Button></Input>
+        );
+
         let ingest_data = this.state.ingest.map((data) => {
             const {capwf,trimmed,locked,secured} = data.wfstatus;
             let id = data.capture_id;
@@ -42,9 +86,23 @@ class WFDBIngest extends Component {
             let capture_src = data.capture_src;
             return (
                 <Table.Row key={id} positive={data.wfstatus.trimmed} warning={!data.wfstatus.capwf} className="monitor_tr">
-                    <Table.Cell>{id}</Table.Cell>
+                    <Popup
+                        trigger={<Table.Cell>{id}</Table.Cell>}
+                        on='click'
+                        hideOnScroll
+                        onOpen={() => this.getStatus(data)}
+                        mountNode={document.getElementById("ltr-modal-mount")}>
+                        {this.props.wf_root ? root : ""}
+                    </Popup>
                     <Table.Cell>{time}</Table.Cell>
-                    <Table.Cell>{secured ? s : ""}{locked ? d : ""}{stop_name}</Table.Cell>
+                    <Popup
+                        trigger={<Table.Cell>{secured ? s : ""}{locked ? d : ""}{stop_name}</Table.Cell>}
+                        on='click'
+                        hideOnScroll
+                        onOpen={() => this.getLine(data)}
+                        mountNode={document.getElementById("ltr-modal-mount")}>
+                        {this.props.wf_root ? week_date : ""}
+                    </Popup>
                     <Table.Cell>{capture_src}</Table.Cell>
                     <Table.Cell>{trimmed ? v : x}</Table.Cell>
                 </Table.Row>
