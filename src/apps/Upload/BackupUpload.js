2e33a4784854c66ecf40a9ecef81a8c37855382f
amnonbb <amnonbb@gmail.com>
2018-09-27 14:09:28 +0300

    BackupUpload: UI progress per file

--- src/apps/Upload/BackupUpload.js
+++ src/apps/Upload/BackupUpload.js
@@ -5,30 +5,44 @@ import {WFSRV_URL} from "../../shared/tools";
 
 class BackupUpload extends Component {
 
-    state = { percent: 0 };
+    state = {
+        progress: {},
+    };
 
     progress = (step, file) => {
+        let {progress} = this.state;
         let count = Math.round(step.percent);
-        //console.log('onProgress', step, file.name);
-        this.setState({percent: count});
+        progress[file.name] = count;
+        this.setState({progress});
     };
 
     uploadDone = (file) => {
+        let {progress} = this.state;
         this.props.onFileData(file);
-        this.setState({percent: 0})
+        console.log("Upload done", file);
+        delete progress[file.file_name];
+        this.setState({progress})
     };
 
     render() {
+
+        const {progress} = this.state;
+
+        let files_progress = Object.keys(progress).map((id) => {
+            let count = progress[id];
+            return (<Progress key={id} label={id} percent={count} indicating progress='percent' />)
+            });
 
         const props = {
             action: `${WFSRV_URL}/backup/upload`,
             type: 'drag',
             accept: '',
+            multiple: true,
             beforeUpload(file) {
-                console.log('beforeUpload', file.name);
+                console.log('beforeUpload', file);
             },
             onStart(file) {
-                console.log('onStart', file.name);
+                console.log('onStart', file);
             },
             onError(err) {
                 console.log('onError', err);
@@ -48,7 +62,7 @@ class BackupUpload extends Component {
                         onProgress={this.progress} >
                         Drop file here or click me
                     </Upload>
-                    <Progress label='' percent={this.state.percent} indicating progress='percent' />
+                    {files_progress}
                 </Message>
             </Segment>
         );
