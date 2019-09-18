8cbe6addd2ca12089d1615cb6e92ff7e15d08eec
amnonbb <amnonbb@gmail.com>
2019-09-18 16:27:50 +0300

    FilesJob: get files list for selected job

--- src/apps/Jobs/FilesJob.js
+++ src/apps/Jobs/FilesJob.js
@@ -1,26 +1,47 @@
 import React, { Component } from 'react';
 import { Container,Message } from 'semantic-ui-react';
 import FilesUpload from "../Upload/FilesUpload";
-import {putData, WFSRV_BACKEND} from "../../shared/tools";
+import {getData, putData, WFSRV_BACKEND} from "../../shared/tools";
 
 class FilesJob extends Component {
 
-    state = {};
+    state = {
+        files: [],
+    };
 
     componentDidMount() {
+        this.getFilesJob()
     };
 
     fileUploaded = (filedata) => {
+        filedata.file_type = "files";
         filedata.archive_type = "files";
         filedata.send_id = this.props.active;
+        filedata.line = {};
+        filedata.line.mime_type = filedata.type;
+        delete filedata.type;
+        delete filedata.url;
         console.log(":: JobFiles - got data: ", filedata);
         putData(`${WFSRV_BACKEND}/workflow/upload`, filedata, (cb) => {
             console.log(":: UploadApp - workflow respond: ",cb);
         });
     };
 
+    getFilesJob = () => {
+        getData(`files/find?key=send_id&value=${this.props.active}`, (files) => {
+            console.log(":: Files DB Data: ",files);
+            this.setState({files});
+        });
+    };
+
 
     render() {
+
+        const {files} = this.state;
+
+        let list_files = files.map((file,i) => {
+            return (<p key={i} >{file.file_id}</p>)
+        });
 
         return (
             <Container textAlign='center' >
@@ -29,9 +50,7 @@ class FilesJob extends Component {
                         Job Files
                     </Message.Header>
                     <FilesUpload onFileData={this.fileUploaded} />
-                    <p>List of job files here</p>
-                    <p>List of job files here</p>
-                    <p>List of job files here</p>
+                    {list_files}
                 </Message>
             </Container>
         );
