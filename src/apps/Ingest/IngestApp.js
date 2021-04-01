f2d87e5cc23e18095e30cd6c1635f6e4bac2e590
amnonbb <amnonbb@gmail.com>
2021-04-01 13:53:02 +0300

    IngestApp: fix bugs

--- src/apps/Ingest/IngestApp.js
+++ src/apps/Ingest/IngestApp.js
@@ -5,7 +5,7 @@ import IngestPresets from "./IngestPresets";
 import LangSelector from "../../components/LangSelector";
 import {getData, newLanguages, putData, WFDB_BACKEND} from "../../shared/tools";
 import moment from "moment";
-import {Label, Segment, Button} from "semantic-ui-react";
+import {Label, Segment, Button, Divider} from "semantic-ui-react";
 
 class IngestApp extends Component {
 
@@ -20,13 +20,16 @@ class IngestApp extends Component {
         this.newCheck();
         let ival = setInterval(() =>
             getData('ingest/find?key=date&value='+moment().format('YYYY-MM-DD'), (data) => {
-                const capture = data.find(c => c.wfstatus.capwf && c.capture_src === "mltcap");
+                const capture = data.find(c => !c.wfstatus.capwf && c.capture_src === "mltcap");
                 const current = Object.assign({}, this.state.capture);
-                if(current.capture_id !== capture.capture_id) {
+                if(current?.capture_id !== capture?.capture_id) {
+                    this.setState({capture})
+                }
+                if(current?.line?.final_name !== capture?.line?.final_name) {
                     this.newCheck();
                     this.setState({capture})
                 }
-            }), 1000);
+            }), 10000);
         this.setState({ival: ival});
     };
 
@@ -78,16 +81,16 @@ class IngestApp extends Component {
 
         return (
             <Fragment>
-                {this.props.admin ? "" : <IngestPresets />}
-                {capture ?
-                <Segment textAlign='center' className="ingest_segment" color='red' raised attached>
-                    <Label  attached='top' className="trimmed_label">{capture_title}</Label>
-                    {capture.line?.final_name !== "" ?
-                        <LangSelector onRef={ref => (this.lang = ref)} onGetLang={this.setLangs}/> : null}
-                    <Button fluid positive disabled={save_disable} onClick={this.saveLang}>Save Languages</Button>
+                {capture?.capture_id ?
+                <Segment textAlign='center' className="ingest_segment" color='red' raised>
+                    <Label attached='top' className="capture_label" icon='record' content={capture_title} />
+                    <Divider />
+                    {capture.line?.final_name !== "" ? <LangSelector onRef={ref => (this.lang = ref)} onGetLang={this.setLangs}/> : null}
+                    {capture.line?.final_name !== "" ? <Button fluid positive disabled={save_disable} onClick={this.saveLang}>Save Languages</Button> : null}
                 </Segment> : null}
                 <IngestTrimmer />
                 <IngestTrimmed />
+                {this.props.admin ? "" : <IngestPresets />}
             </Fragment>
         );
     }
