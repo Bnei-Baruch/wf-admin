14aa118959caf90bdf7edd568ac2b246a1bbe8bd
amnonbb <amnonbb@gmail.com>
2021-08-05 17:54:55 +0300

    UserManager: refactor and clean

--- src/components/UserManager.js
+++ src/components/UserManager.js
@@ -4,65 +4,64 @@ import mqtt from "../shared/mqtt";
 const userManagerConfig = {
     url: 'https://accounts.kab.info/auth',
     realm: 'main',
-    clientId: 'wf-admin',
-    scope: 'profile',
-    enableLogging: true,
+    clientId: 'wf-admin'
+};
+
+const initOptions = {
+    onLoad: "check-sso",
+    checkLoginIframe: false,
+    flow: "standard",
+    pkceMethod: "S256",
+    enableLogging: true
 };
 
 export const kc = new Keycloak(userManagerConfig);
 
 kc.onTokenExpired = () => {
-    console.debug(" -- Renew token -- ");
     renewToken(0);
 };
 
 kc.onAuthLogout = () => {
-    console.debug("-- Detect clearToken --");
+    console.log("-- LogOut --");
     mqtt.setToken(null);
     kc.logout();
 }
 
 const renewToken = (retry) => {
-    kc.updateToken(70)
+    kc.updateToken(5)
         .then(refreshed => {
             if(refreshed) {
-                document.cookie = `token=${kc.token};`;
-                console.debug("-- Refreshed --");
-                mqtt.setToken(kc.token);
+                setData();
             } else {
                 console.warn('Token is still valid?..');
             }
         })
-        .catch(err => {
+        .catch(() => {
             retry++;
-            if(retry > 5) {
-                console.error("Refresh retry: failed");
-                console.debug("-- Refresh Failed --");
+            if(retry > 10) {
                 kc.clearToken();
             } else {
                 setTimeout(() => {
-                    console.error("Refresh retry: " + retry);
                     renewToken(retry);
                 }, 10000);
             }
         });
 }
 
-
+const setData = () => {
+    document.cookie = `token=${kc.token};`;
+    const {realm_access: {roles}} = kc.tokenParsed;
+    let user = {...kc.tokenParsed, token: kc.token, roles};
+    mqtt.setToken(kc.token);
+    return user;
+}
 
 export const getUser = (callback) => {
-    kc.init({onLoad: 'check-sso', checkLoginIframe: false, flow: 'standard', pkceMethod: 'S256'})
+    kc.init(initOptions)
         .then(authenticated => {
-            if(authenticated) {
-                document.cookie = `token=${kc.token};`;
-                const {realm_access: {roles}} = kc.tokenParsed;
-                let user = {...kc.tokenParsed, token: kc.token, roles};
-                mqtt.setToken(kc.token);
-                callback(user)
-            } else {
-                callback(null)
-            }
-        }).catch((err) => console.log(err));
+            const user = authenticated ? setData() : null;
+            callback(user);
+        }).catch(err => console.error(err));
 };
 
 export default kc;
